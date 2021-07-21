6eac8a291e3d83ad79798cd4e7b38a7b748276b5
ellisp97 <ellispridgeon1@gmail.com>
2021-07-21 22:44:49 +0100

    dockerfile test

--- main.go
+++ main.go
@@ -14,13 +14,10 @@ func check(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Health Check</h1>")
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<html><h2>Hey there, thanks for entering %s!</h2></html>", r.URL.Path[1:])
-}
-
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/health_check", check)
 	fmt.Println("Server starting...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":3000", nil))
 
 }
